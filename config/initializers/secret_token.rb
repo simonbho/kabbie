32727f509fd2724421bdd06c58b740cb9820e1fd
simonbho <sho2015@kellogg.northwestern.edu>
2014-02-05 12:56:19 -0600

    kabbie
    first app build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kabbie::Application.config.secret_key_base = '062b4c6690e2ab9add2119980a46e16ae65b473d9af945b15efe73ff49106122ddebf06ee5a12ef4d6fc2abc49a0b4b1757dee9ee868750816085e0872646c04'
